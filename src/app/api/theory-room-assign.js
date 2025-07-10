c68bfdd4923b85c97caf99e757d8d23872a1e250
AfzalHossan-2005021 <afzalhossan2005021@gmail.com>
2025-07-11 03:58:58 +0600

    Theory room assignment added.

---
+++ src/app/api/theory-room-assign.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+import { api_url } from ".";
+
+export const getAllTheoryRoomAssignment = () =>
+  axios.get(api_url("/theory_room_assignment/get/all")).then((res) => res.data);
+
+export const updateTheoryRoomAssignment = (assignment) =>
+  axios.post(api_url(`/theory_room_assignment/update`), assignment).then((res) => res.data);
+
+export const getAllSectionRoomAllocation = () =>
+  axios.get(api_url("/theory_room_assignment/section/get/all")).then((res) => res.data);
+
+export const updateSectionRoomAllocation = (allocation) =>
+  axios.put(api_url(`/theory_room_assignment/section/update`), allocation).then((res) => res.data);
